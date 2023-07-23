2ec688cab3c1002907632e01acb24eeed0f44150
Daniela Koleva <dkoleva471@gmail.com>
2023-07-23 20:41:03 +0300

    Added ex. 3 and 4

---
+++ 3.  Aretmetic operations/Program.cs
@@ -0,0 +1,39 @@
+﻿//int
+Console.WriteLine("Резултати в тип int");
+int a = 2 / 5;
+Console.WriteLine(a);
+int b = 11/5;
+Console.WriteLine(b);
+int c = 15 % 2;
+Console.WriteLine(c);
+int d = (int)(31 * 0.5);
+Console.WriteLine(d);
+int e = (int) (21.5 - 5);
+Console.WriteLine(e);
+int f = (int)(35 - 20.1);
+Console.WriteLine(f);
+int g = 30 - 20;
+Console.WriteLine(g);
+int h = 10 - 15;
+Console.WriteLine(h);
+// double
+Console.WriteLine("Резултати в тип double");
+double a1 = 2 / 5;
+Console.WriteLine(a1);
+double b1 = (double) (11 / 5);
+Console.WriteLine(b1);
+double c1 = 15 % 2;
+Console.WriteLine(c1);
+double d1 = 31 * 0.5;
+Console.WriteLine(d1);
+double e1 = 21.5 - 5;
+Console.WriteLine(e1);
+double f1 = 35 - 20.1;
+Console.WriteLine(f1);
+double g1 = (double)(30 - 20);
+Console.WriteLine(g1);
+double h1 = (double)(10 - 15);
+Console.WriteLine(h1);
+
+
+
---
+++ 4. Math Class/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int a = 5;
+int b = 8;
+double c = 10.786;
+Console.WriteLine("По-голямото число е: " + Math.Max(a, b));
+Console.WriteLine("По-малкото число е: "+ Math.Min(a,b));
+Console.WriteLine("Корен квадратен от а е: "+ Math.Sqrt(a));
+Console.WriteLine("Числото a повдигнато на 2ра степен е равно на:" + Math.Pow(a, 2));
+Console.WriteLine("Абсолютната стойнност на числото а е: " + Math.Abs(a));
+Console.WriteLine("Числото c, закръглено до втори знаек е: " + Math.Round(c,2));
+
+
