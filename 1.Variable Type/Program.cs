b5ec661e99dcea08da77ca8ed26c7c31b2c95e69
Daniela Koleva <dkoleva471@gmail.com>
2023-07-23 20:02:58 +0300

    Added exercises 1 and 2

--- 1.Variable Type/Program.cs
+++ 1.Variable Type/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Pesho";
+Console.WriteLine(name);
+int number = 5;
+Console.WriteLine(number);
+double number2= 3.14;
+Console.WriteLine(number2);
+long number3 = 088711133424;
+Console.WriteLine(number3);
+bool number4 = 5 > 3;
+Console.WriteLine(number4);
+char symbol = 'Z';
+Console.WriteLine(symbol);
+
---
+++ Real Numbers/Program.cs
@@ -0,0 +1,6 @@
+﻿double a = 0.3 + 0.3 + 0.3;
+Console.WriteLine(a);
+decimal b =0.3m + 0.3m + 0.3m;
+Console.WriteLine(b);
+float c = 0.3f + 0.3f + 0.3f;
+Console.WriteLine(c);
