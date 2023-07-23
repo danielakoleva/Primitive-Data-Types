73de8cb633556b791a4f8c6f22b4b5cc2044c064
Daniela Koleva <dkoleva471@gmail.com>
2023-07-23 22:27:14 +0300

    Added exercises 10 and 11.

---
+++ 10.  Math/Program.cs
@@ -0,0 +1,9 @@
+﻿int a =int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int d = int.Parse(Console.ReadLine());
+int sum = a + b;
+int multiplication = c * d;
+double result = sum/multiplication;
+Console.WriteLine(result);
+
---
+++ 11.  Math functions/Program.cs
@@ -0,0 +1,6 @@
+﻿double a = Math.Sqrt(8 + 17) + Math.Pow(3,2);
+double b = 7 + Math.Sqrt(25);
+double result1 = a / b;
+double result2 = Math.Sqrt(Math.Pow(7,2)+ Math.Pow(2,2));
+double result = result1 - 21 + result2;
+Console.WriteLine(result);
